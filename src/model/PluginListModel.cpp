e76e01a46e3db5069713030c91aa2b8ea36a778b
Marcin Kolny <marcin.kolny@gmail.com>
2013-10-07 20:15:17 +0200

    implemented listing all available elements

--- src/model/PluginListModel.cpp
+++ src/model/PluginListModel.cpp
@@ -6,6 +6,10 @@
  */
 
 #include "PluginListModel.h"
+#include <glibmm.h>
+
+using namespace Gst;
+using Glib::RefPtr;
 
 PluginListModel::PluginListModel(QObject *parent)
 : QAbstractItemModel(parent)
@@ -103,8 +107,73 @@ int PluginListModel::rowCount(const QModelIndex &parent) const
 	return parent_item->child_count();
 }
 
+#include <gst/gst.h>
 void PluginListModel::setup_model_data()
 {
+	RefPtr<Registry> registry = Registry::get();
+
+	Glib::ListHandle<RefPtr<Plugin>> plugins = registry->get_plugin_list();
+	int blacklist_count = 0,
+			feature_count = 0;
+
+	for (auto plugin : plugins)
+	{
+		if (GST_OBJECT_FLAG_IS_SET (plugin->gobj(), GST_PLUGIN_FLAG_BLACKLISTED))
+		{
+			blacklist_count++;
+			continue;
+		}
+
+		Glib::ListHandle<RefPtr<PluginFeature>> features = registry->get_feature_list(plugin->get_name());
+
+		for (auto feature : features)
+		{
+			if (!feature)
+				continue;
+
+			feature_count++;
+
+			if (GST_IS_ELEMENT_FACTORY (feature->gobj()))
+			{
+				RefPtr<ElementFactory> factory = factory.cast_static(feature);
+
+				g_print ("%s:  %s: %s\n", plugin->get_name().c_str(),
+						factory->get_name().c_str(),
+						factory->get_metadata(GST_ELEMENT_METADATA_LONGNAME).c_str());
+			}
+			else if (GST_IS_TYPE_FIND_FACTORY (feature->gobj()))
+			{
+				RefPtr<TypeFindFactory> factory = factory.cast_static(feature);
+
+				g_print ("%s: %s: ", plugin->get_name().c_str(),
+						feature->get_name().c_str());
+
+				std::vector<Glib::ustring> extensions = factory->get_extensions();
+
+				if (!extensions.empty())
+				{
+					guint i = 0;
+
+					for (auto extension : extensions)
+					{
+						g_print ("%s%s", i > 0 ? ", " : "", extension.c_str());
+						i++;
+					}
+					g_print ("\n");
+				}
+				else
+				{
+					g_print ("no extensions\n");
+				}
+			}
+			else
+			{
+				g_print ("%s:  %s (%s)\n", plugin->get_name().c_str(),
+						feature->get_name().c_str(), feature->get_name().c_str());
+			}
+		}
+	}
+
 	QList<ElementItem*> parents;
 	QList<int> indentations;
 	parents << root_item;
