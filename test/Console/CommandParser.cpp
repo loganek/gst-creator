058e3bbcc761ad043f1dc6b05e2f8050ae81fa0c
Marcin Kolny <marcin.kolny@gmail.com>
2013-10-31 22:52:15 +0100

    added test module

---
+++ test/Console/CommandParser.cpp
@@ -0,0 +1,15 @@
+/*
+ * CommandParser.cpp
+ *
+ *  Created on: 31 pa? 2013
+ *      Author: Marcin Kolny
+ */
+
+#include <gtest/gtest.h>
+
+TEST(CommandParser, DummyTest)
+{
+	ASSERT_TRUE(true);
+}
+
+
---
+++ test/UTmain.cpp
@@ -0,0 +1,17 @@
+/*
+ * UTmain.cpp
+ *
+ *  Created on: 31 pa? 2013
+ *      Author: Marcin Kolny
+ */
+
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+
+	return RUN_ALL_TESTS();
+}
+
+
